f378d8932b65fd3b3d0ead489732c14aa74358f5
LAG Florian Thalmann <Florian.Thlamann@leuchterag.ch>
2020-04-18 11:43:57 +0200

    Added simple guid validator for client and added azurerequest model

---
+++ src/CalendarExtractor.Web.Client/Data/AzureRequestModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CalendarExtractor.Web.Client.Data.Validation;
+
+namespace CalendarExtractor.Web.Client.Data
+{
+    public class AzureRequestModel
+    {
+        [Required(AllowEmptyStrings = false)]
+        [GuidValidator]
+        public string ClientId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [GuidValidator]
+        public string ClientSecret { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [GuidValidator]
+        public string TenantId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string CalendarId { get; set; }
+    }
+}
---
+++ src/CalendarExtractor.Web.Client/Data/Validation/GuidValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CalendarExtractor.Web.Client.Data.Validation
+{
+    public class GuidValidator : ValidationAttribute
+    {
+        public const string DefaultErrorMessage = "{0} not a valid GUID";
+        public GuidValidator() : base(DefaultErrorMessage) { }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            if (value is string input)
+            {
+                if (Guid.TryParse(input, out var _))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
