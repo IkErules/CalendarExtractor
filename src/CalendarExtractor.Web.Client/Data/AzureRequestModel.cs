70108eeeef0509f5a82ee43adc84becc52e48945
LAG Florian Thalmann <Florian.Thlamann@leuchterag.ch>
2020-04-18 13:16:48 +0200

    Client runs acainst azure grpc server now

--- src/CalendarExtractor.Web.Client/Data/AzureRequestModel.cs
+++ src/CalendarExtractor.Web.Client/Data/AzureRequestModel.cs
@@ -7,17 +7,19 @@ namespace CalendarExtractor.Web.Client.Data
     {
         [Required(AllowEmptyStrings = false)]
         [GuidValidator]
-        public string ClientId { get; set; }
+        public string ClientId { get; set; } = "816c3a23-7568-40a1-b12a-c65795fcbd92";
+
+        [Required(AllowEmptyStrings = false)]
+        public string ClientSecret { get; set; } = "my-g4z-t4.M-zOR8OBBrkjq66=UtK8T1";
 
         [Required(AllowEmptyStrings = false)]
         [GuidValidator]
-        public string ClientSecret { get; set; }
+        public string TenantId { get; set; } = "0dcc5a15-9225-4982-9557-c7f8d345bd63";
 
         [Required(AllowEmptyStrings = false)]
-        [GuidValidator]
-        public string TenantId { get; set; }
+        public string CalendarId { get; set; } = "meetingroombig@flotha.onmicrosoft.com";
 
-        [Required(AllowEmptyStrings = false)]
-        public string CalendarId { get; set; }
+        [Required]
+        public int Minutes { get; set; }
     }
 }
--- src/CalendarExtractor.Web.Client/Data/AzureService.cs
+++ src/CalendarExtractor.Web.Client/Data/AzureService.cs
@@ -1,12 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using CalendarExtractor.API;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
 
 namespace CalendarExtractor.Web.Client.Data
 {
     public class AzureService
     {
+        private const string ServerAddress = "http://localhost:5000";
 
+        public async Task<IEnumerable<AzureReplyEvent>> ReadCalendarEventsFor(AzureRequestModel azureRequest)
+        {
+            var calendar = CreateCalendar(azureRequest.Minutes, azureRequest.CalendarId);
+            if (calendar == null) return new List<AzureReplyEvent>();
+
+            var request = CreateAzureRequest(calendar, azureRequest);
+
+            // This switch must be set before creating the GrpcChannel/HttpClient for calls without TLS connection.
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            using var channel = GrpcChannel.ForAddress(ServerAddress);
+            var azureClient = new Azure.AzureClient(channel);
+
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+
+            using var streamingCall = azureClient.GetCalendarInformation(request, cancellationToken: cts.Token);
+
+            //using var streamingCall = isTest
+            //    ? azureClient.TestGetCalendarInformation(request, cancellationToken: cts.Token)
+            //    : azureClient.GetCalendarInformation(request, cancellationToken: cts.Token);
+
+            var replyEventList = new List<AzureReplyEvent>();
+
+            try
+            {
+                await foreach (var replyEvent in streamingCall.ResponseStream.ReadAllAsync(cts.Token))
+                {
+                    replyEventList.Add(replyEvent);
+                }
+
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+            {
+                Console.WriteLine("Stream cancelled because of cancellation token.");
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine($"Exception occured: {ex}");
+            }
+
+            return replyEventList;
+        }
+
+        private AzureRequest.Types.Calendar CreateCalendar(int minutes, string calendarId)
+        {
+            var startTime = DateTime.Now;
+            var endTime = startTime.AddMinutes(minutes);
+
+            Console.WriteLine("Beginn Abfrage: " + startTime.ToString("g"));
+            Console.WriteLine("Ende Abfrage: " + endTime.ToString("g"));
+
+            return new AzureRequest.Types.Calendar
+            {
+                CalendarId = calendarId,
+                BeginTimestamp = CreateTimestampOf(startTime),
+                EndTimestamp = CreateTimestampOf(endTime)
+            };
+        }
+        
+        private AzureRequest CreateAzureRequest(AzureRequest.Types.Calendar calendar, AzureRequestModel model)
+        {
+
+            return new AzureRequest
+            {
+                Client = new AzureRequest.Types.Client
+                {
+                    ClientId = model.ClientId,
+                    ClientSecret = model.ClientSecret,
+                    TenantId = model.TenantId,
+                },
+                Calendar = calendar
+            };
+        }
+
+        private Timestamp CreateTimestampOf(DateTime dateTime)
+        {
+            DateTimeOffset offset = dateTime;
+
+            return offset.ToTimestamp();
+        }
     }
 }
